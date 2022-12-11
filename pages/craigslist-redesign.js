da76021dbcf423feb7a28ea9e59a82ae98d08767
IchigoBlues <atif_khan@brown.edu>
2022-12-11 14:32:59 -0500

    a

---
+++ pages/craigslist-redesign.js
@@ -0,0 +1,5 @@
+function Craigslist() {
+    return <h1>Craigslist Page</h1>
+}
+
+export default Craigslist
--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,8 @@ import {
 } from "react-icons/ai";
 import { BsFillMoonStarsFill } from "react-icons/bs";
 import { useState } from "react";
-// import kon from "/kon4.png";
-// import code from "/code.png";
-// import design from "../../public/design.png";
 import Image from "next/image";
-// import web1 from "../../public/briefcase.png";
-// import web2 from "../../public/craigslist.png";
-// import web3 from "../../public/myanimelist.png";
-// import web4 from "../../public/vacation_homerents.png";
+
 
 
 export default function Home() {
